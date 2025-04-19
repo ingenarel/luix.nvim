433f76287726cdba0d1f2b6411d386598f714cf9
ingenarel <ingenarelitems@gmail.com>
2025-04-20 01:24:05 +0600

    (misc): add format

--- lua/luix/init.lua
+++ lua/luix/init.lua
@@ -1,32 +1,32 @@
 local m = {}
 
 function m.parse(inputs, outputs)
-	local file = ""
+    local file = ""
 
-	file = file .. "{ "
-	for i = 1, #inputs - 1 do
-		file = file .. inputs[i] .. ", "
-	end
-	file = file .. inputs[#inputs] .. " }: {\n"
+    file = file .. "{ "
+    for i = 1, #inputs - 1 do
+        file = file .. inputs[i] .. ", "
+    end
+    file = file .. inputs[#inputs] .. " }: {\n"
 
-	if type(outputs.systemPackages) == "table" then
-		file = file .. "    environment.systemPackages = with pkgs; [\n"
-		for i = 1, #outputs.systemPackages do
-			file = file .. "        " .. outputs.systemPackages[i] .. "\n"
-		end
-		file = file .. "    ];\n"
-	end
+    if type(outputs.systemPackages) == "table" then
+        file = file .. "    environment.systemPackages = with pkgs; [\n"
+        for i = 1, #outputs.systemPackages do
+            file = file .. "        " .. outputs.systemPackages[i] .. "\n"
+        end
+        file = file .. "    ];\n"
+    end
 
-	file = file .. "}"
-	return file
+    file = file .. "}"
+    return file
 end
 
 function m.saveFile(contents, filePath)
-	local file = io.open(vim.fs.normalize(filePath), "w")
-	if file ~= nil then
-		file:write(contents)
-		file:close()
-	end
+    local file = io.open(vim.fs.normalize(filePath), "w")
+    if file ~= nil then
+        file:write(contents)
+        file:close()
+    end
 end
 
 return m
